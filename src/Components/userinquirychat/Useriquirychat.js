c99111b780f081b0dda01de710cd27bd6ed2a0a7
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-06-20 18:34:45 +0530

    kgf8

--- src/Components/userinquirychat/Useriquirychat.js
+++ src/Components/userinquirychat/Useriquirychat.js
@@ -78,9 +78,9 @@ export default function Userinquirychat() {
         console.log('socket messae  ....');
         console.log('inquiryId',item.item._id);
         
-        // if (newMessage.inquiryId == item._id) {
+        if (newMessage.inquiryId == item._id) {
           setchat((prevChat) => [...prevChat, newMessage]);
-        // }
+        }
       });
 
       return () => socket?.off("newMessage");
