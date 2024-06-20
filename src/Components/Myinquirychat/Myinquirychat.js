79bd7da3294852cfc1b18d78699e64b533c707ba
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-06-20 17:38:40 +0530

    kgf5

--- src/Components/Myinquirychat/Myinquirychat.js
+++ src/Components/Myinquirychat/Myinquirychat.js
@@ -78,7 +78,7 @@ export default function Userinquirychat() {
         console.log('socket messae  ....');
         console.log('inquiryId',item.item._id);
         
-        if (newMessage.inquiryId == item._id) {
+        if (newMessage.inquiryId == item.item._id) {
           setchat((prevChat) => [...prevChat, newMessage]);
         }
       });
@@ -126,7 +126,7 @@ export default function Userinquirychat() {
   console.log("item:", item);
 
   
-  const [inquiryId, setid] = useState(item._id);
+  const [inquiryId, setid] = useState(item.item._id);
   // console.log(inquiryId);
   const [receiverId, setReceiverId] = useState(item.item.userData._id)
   
