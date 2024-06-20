3ae77dedd712af15e4add407b764ec7419b15f65
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-06-20 11:39:25 +0530

    dependency removed

--- src/Components/Myinquirychat/Myinquirychat.js
+++ src/Components/Myinquirychat/Myinquirychat.js
@@ -103,7 +103,7 @@ export default function Myinquirychat() {
 
   useEffect(() => {
     display();
-  }, [chat]);
+  }, []);
 
   const scrollToBottom = () => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
