e09722829bf7c9cd5b50b8998fb010c349a52882
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-06-25 14:55:20 +0530

    new commit

--- src/Container/HomePage/HomePage.js
+++ src/Container/HomePage/HomePage.js
@@ -7,6 +7,7 @@ import NewsAll from '../../Components/NewsAll/NewsAll'
 import Question from '../../Components/Question/Question'
 import HeroSection from '../../Components/HeroSection/HeroSection'
 import LoginPage from '../../Components/Login/LoginPage'
+import "./home.css"
 import AddStartUp from '../../Components/AddStartUp/Addprduct'
 import DisplayProfileWithStartupList from '../../Components/DisplayProfileWithStartupList/DisplayProfileWithStartupList'
 import DisplayProfile from '../../Components/DisplayProfile/DisplayProfile'
